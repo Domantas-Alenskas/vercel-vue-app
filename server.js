9854857be299e52bda3f432b0033e8b6bfbc120a
Domantas Alenskas <domantas.alenskas@hostinger.com>
2025-03-28 16:17:55 +0200

    change port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3020;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
